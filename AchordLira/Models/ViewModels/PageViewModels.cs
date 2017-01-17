46abd31a793081907e70ea42442de670b5a17f79
Stefan <stefan.stamenovic@gmail.com>
2017-01-17 14:32:53 +0100

    Fix ne commitovane stvari.

---
+++ AchordLira/Models/ViewModels/PageViewModels.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AchordLira.Models.ViewModels
+{
+    //---------------------------------------------------------------------------------------------//
+    //Main page model class
+    public class PageViewModel
+    {
+        //Connected user
+        public ViewUser user = null;
+        //List of authors
+        public Dictionary<String, List<ViewArtist>> artists { get; set; }
+        public List<String> genres;
+        //Selected genre
+        public String genre;
+    }
+    //---------------------------------------------------------------------------------------------//
+    // Home/Index page model
+    public class HomePageViewModel:PageViewModel
+    {
+        public List<ViewSong> popularSongs { get; set; }
+        public List<ViewSongRequest> songRequests;
+        public List<ViewSong> latestSongs;
+    }
+    //---------------------------------------------------------------------------------------------//
+    //Song page model
+    public class SongPageViewModel : PageViewModel
+    {
+        public string artist { get; set; }
+        public ViewSong song { get; set; }
+        public List<ViewComment> messages { get; set; }
+        public List<ViewSong> songs { get; set; }
+    }
+    //---------------------------------------------------------------------------------------------//
+    //User page model
+    public class UserPageViewModel : PageViewModel
+    {
+        public List<ViewSong> userSongs { get; set; }
+        public List<ViewSong> favoritSongs { get; set; }
+    }
+}
---
+++ AchordLira/Models/ViewModels/ViewArtist.cs
@@ -0,0 +1,24 @@
+﻿using AchordLira.Models.Neo4J.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AchordLira.Models.ViewModels
+{
+    public class ViewArtist
+    {
+        public string name { get; set; }
+        public string link { get; set; }
+        public string biography { get; set; }
+        public string website { get; set; }
+
+        public ViewArtist(Artist artist)
+        {
+            name = artist.name;
+            link = artist.link;
+            biography = artist.biography;
+            website = artist.website;
+        }
+    }
+}
