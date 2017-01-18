ebf0c02b2a1a6dd21a02399eb2e6d1466c495a3f
Stefan <stefan.stamenovic@gmail.com>
2017-01-18 02:16:22 +0100

    Dodato sto nije praceno

---
+++ AchordLira/Models/Neo4J/Models/SongDraft.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AchordLira.Models.Neo4J.Models
+{
+    public class SongDraft
+    {
+        public string artist { get; set; }
+        public string name { get; set; }
+        public string content { get; set; }
+        public string date { get; set; }
+        public string link { get; set; }
+    }
+}
